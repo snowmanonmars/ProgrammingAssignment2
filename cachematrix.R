2481c1fca8df4a8bb932dd720e2fa72362b053c3
snowmanonmars <snowmanonmars@gmail.com>
2017-04-24 03:57:11 +0900

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,7 @@
 ## makeCacheMatrix is a function that creates a list of closures, 
 ## functions made by a function.
 ## In specific, 1) it sets a matrix, 2) get the values of it,
-## 3) sets an inverse of the matrix set (by help of the function cacheSolve in this assignment)
+## 3) sets an inverse of the matrix (by help of the function cacheSolve in this assignment)
 ## and 4) get the values of it. 
 ## By assigning the call of makeCacheMatrix to an symbol, 
 ## e.g. m1 <-makeCacheMatrix(matrix(1:4,2,2)), 
@@ -10,7 +10,7 @@
 ## with the execution environment fixed for further reference, 
 ## i.e. containing the variables such as x and inv.
 ## (while normally the execution environment should disappear after evaluation 
-## of the function.)
+## of the function, here it becomes the enclosing environment of the four closures)
 ## Alternatively, you can start with m2 <- makeCacheMatrix(), which will make NULL x and inv,
 ## and use subsetting such as m2$setmatrix(matrix(5:8, 2, 2)) for settings.
 
@@ -32,9 +32,9 @@ makeCacheMatrix <- function(x = matrix()){
 
 ## By passing m1 to cacheSolve,
 ## the function cacheSolve accesses the functions in the list of m1 and use them 
-## to check if a cached inv value exists in the reference environment, 
-## return it if it exists
-## or calculate and assign it to inv in the reference environment as new.
+## to check if a cached inv value exists in the reference environment, i.e. the enclosing environment of the above four closures, 
+## and return the cached inv if it exists.
+## Otherwise the following statments will calculate and assign a inverse to inv in the reference environment.
 ## Look the mentions inside the code
 
 cacheSolve <- function(x, ...){
@@ -43,7 +43,7 @@ cacheSolve <- function(x, ...){
         message("getting cached data")
         return(inv)
         ## Return a matrix that is the inverse of 'x' when it is cached in the
-        ## environment of reference
+        ## referenece environment.
     }
     matrix <- x$getmatrix()
     inv <- solve(matrix)
